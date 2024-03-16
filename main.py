265da36e4eb3da7566dbe76b76041ad9c27ddd67
janlaskowski <jan.john.laskowski@gmail.com>
2024-03-16 17:11:39 +0100

    refactoring code for readability

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = open_file(book_path)
-    
     count_words = get_num_words(text)
-
     counter_letters = count_letter(text)
-    
     char_list = dict_to_list(counter_letters)
-
-    print(f"The number of words is {count_words}")
-    
     char_list.sort(reverse=True, key=sort_on)
-
+    
+    #printing the report of the text
+    print(f"The number of words is {count_words}")
     for element in char_list:
         print(f"The '{element["name"]}' character was found '{element["num"]}'")
     
-
+#changing dictinary to list of dictinaries
 def dict_to_list(dict):
-    
     list = []
-
 
     for element in dict:
         if element.isalpha():
@@ -27,18 +21,19 @@ def dict_to_list(dict):
 
     return list
 
+#showing sort function where to sort
 def sort_on(dict):
     return dict["num"]
 
+#opening the file and reading its content
 def open_file(path):
     with open(path) as f:
         return f.read()
 
+#couting the letters in text
 def count_letter(text):
     counter_letter = {}
-
     lowered_text = text.lower()
-
     chars = list(lowered_text)
 
     for char in chars:
@@ -49,10 +44,9 @@ def count_letter(text):
 
     return counter_letter
 
+#getting the number of words from list
 def get_num_words(text):
     words = text.split()
     return len(words)
-
-    print(counter)
 
 main()
